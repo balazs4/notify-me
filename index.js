6618f78ada45b2743d4a1feabd61694e8aeae6aa
balazs4 <balazs4web@gmail.com>
2018-01-14 15:30:27 +0100

    feat: remove duplicate check
    + add better fallback image
    + upgrade dependencies

--- index.js
+++ index.js
@@ -12,60 +12,30 @@ const notify = options =>
     });
   });
 
-const download = (
-  url = 'http://icons.iconarchive.com/icons/elegantthemes/beautiful-flat/128/radio-icon.png'
-) => {
-  if (!url) {
-    return undefined;
-  }
-  return new Promise(resolve => {
-    const base64 = new Buffer(url).toString('base64');
-    const file = path.join(os.tmpdir(), `${base64}.png`);
-    if (fs.existsSync(file)) {
-      resolve(file);
-      return;
-    }
+const download = url =>
+  new Promise(resolve => {
+    const fallback = 'notification-audio-play';
+    if (!url) resolve(fallback);
+    const file = path.join(os.tmpdir(), `notify-me.png`);
     request
       .get(url)
       .pipe(fs.createWriteStream(file))
       .on('error', () => {
-        resolve(undefined);
+        resolve(fallback);
       })
       .on('finish', () => {
         resolve(file);
       });
   });
-};
-
-const marker = path.join(os.tmpdir(), 'notifyme.json');
 
 module.exports = ({ publish }) => async ({ payload, topic }) => {
   try {
     const { title, body, iconUrl } = payload;
-
     const icon = await download(iconUrl);
-
-    const notification = {
+    await notify({
       title,
       message: body || new Date().toLocaleString(),
       icon
-    };
-    const previous = await new Promise(resolve => {
-      fs.readFile(marker, (err, data) => {
-        if (err) resolve('');
-        else resolve(data.toString());
-      });
-    });
-
-    if (JSON.stringify(notification) === previous) {
-      return;
-    }
-
-    await notify(notification);
-    await new Promise(resolve => {
-      fs.writeFile(marker, JSON.stringify(notification), err => {
-        resolve();
-      });
     });
   } catch (err) {
     console.error(err.message);
